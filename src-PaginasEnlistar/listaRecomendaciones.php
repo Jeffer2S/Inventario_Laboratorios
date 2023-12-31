1fe86b307472022dc550f4f7c683645f56e2e349
Alex Lizano <alexsanty10lm@gmail.com>
2023-12-31 11:19:30 -0500

    Se realizo la codificación para mostrar las recomendaciones en una tabla

--- src-PaginasEnlistar/listaRecomendaciones.php
+++ src-PaginasEnlistar/listaRecomendaciones.php
@@ -4,8 +4,33 @@
     INNER JOIN laboratorios AS l ON r.num_lab_per = l.num_labo";
     $result=$conn->query($consulta);
     if ($result){
+        ?>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="row"></th>
+                        <th scope="col">Laboratorio</th>
+                        <th scope="col">Recomendación</th>
+                    </tr>
+                </thead>
+                <tbody>
+        <?php
         while ($row=$result->fetch_array()){
-
+            $num_rec = $row['r.num_rec'];
+            $nom_lab = $row['l.nom_labo'];
+            $des_rec = $row['r.des_rec'];
+            ?>
+                <td><?php echo $num_rec?></td>
+                <td><?php echo $nom_lab?></td>
+                <td><?php echo $des_rec?></td>
+            <?php
+            $cont++;
         }
+        ?>
+            </tbody>
+            </table>
+        </div>
+        <?php
     }
 ?>
