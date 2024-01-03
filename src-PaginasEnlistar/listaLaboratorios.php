9c04fe0128c9a8a9a900922489b92201e5289d76
Alex Lizano <alexsanty10lm@gmail.com>
2024-01-03 10:04:26 -0500

    Se modificó la consulta a la bd y se modificaron los valores de la tabla de laboratorios

--- src-PaginasEnlistar/listaLaboratorios.php
+++ src-PaginasEnlistar/listaLaboratorios.php
@@ -1,9 +1,8 @@
 <?php
-    require './src-PaginasEnlistar/Mysql/conexion.php';    
-    $consulta="SELECT num_labo, nom_labo, pis_labo FROM laboratorios";
-    $consultaLabor="SELECT nom_lab, ape_lab FROM laboratoristas WHERE 
-    ced_lab in (SELECT ced_lab_enc FROM laboratorios WHERE num_lab=$num_lab";
-    $consultaPc="SELECT * FROM computadoras where num_lab_per=$num_lab";
+    require 'Mysql/conexion.php';    
+    $consulta=" SELECT l.*, u.apellido, u.nombre
+    FROM laboratorios AS l
+    INNER JOIN usuarios AS u ON l.laboratorista_encargado=u.id";
     $result=$conn->query($consulta);
     if ($result){
         ?>
@@ -14,32 +13,33 @@
         <div class="contenedor">
         <?php
         while ($row=$result->fetch_array()){
-            $num_lab = $row['num_labo'];
-            $nom_lab = $row['nom_labo'];
-            $pis_lab = $row['pis_labo'];
-
+            $num_lab = $row['id_lab'];
+            $nom_lab = $row['nom_lab'];
+            $pis_lab = $row['piso_lab'];
+            $lab_enc = $row['apellido'].' '.$row['nombre'];
             ?>
-                <div class="cuadro-contenedor" onclick="mostrarInformacion(<?php $num_lab?>, this)">
+                <div class="cuadro-contenedor" onclick="mostrarInformacion(<?php echo $num_lab?>, this)">
                 <div> 
                     <?php echo $nom_lab?>
                     <?php echo $pis_lab?>
-                    <p>Laboratoristas Encargados:</p>
-                    <?php $result1=$conn->query($consultaLabor);
-                    while ($row1=$result1->fetch_array()){
-                      echo $row1['nom_lab'];
-                      echo $row1['ape_lab'];  
-                    }
+                    <p>Laboratorista Encargado:</p>
+                    <?php
+                    echo $lab_enc;
                     ?>
                 </div>
                 <div class="informacionAdicional">
                 <h2><?php echo $nom_lab?></h2>
                 <div class="cuadrosAdicionales">
-                <?php $result2=$conn->query($consultaPc);
+                <?php 
+                $consulta_bienes = " SELECT * FROM bienes
+                WHERE id_lab_per= $num_lab
+                ORDER BY tipo_bien";
+                $result2=$conn->query($consulta_bienes);
                     while($row2=$result2->fetch_array()){
                         ?> <div class="cuadroAdicional">
-                            <?php echo $row2['num_Con']?>
-                            <?php echo $row2['cod_Con']?>
-                            <?php echo $row2['mar_Con']?>
+                            <?php echo $row2['tipo_bien']?>
+                            <?php echo $row2['id_bien']?>
+                            <?php echo $row2['est_bien']?>
                         </div><?php
                     }
                 ?>
@@ -51,29 +51,6 @@
             ?>
             </div>
             </div>
-            <script>
-                let cuadroSeleccionado = 0;
-                let cuadroActual = null;
-
-                function mostrarInformacion(numeroCuadro, cuadroContenedor) {
-                const informacionAdicionales = document.querySelectorAll('.informacionAdicional');
-
-                if (cuadroSeleccionado === numeroCuadro) {
-                    cuadroSeleccionado = 0;
-                    cuadroActual.classList.remove('destacado');
-                    cuadroActual = null;
-                } else {
-                    cuadroSeleccionado = numeroCuadro;
-
-                    informacionAdicionales.forEach(info => {
-                    info.parentNode.classList.remove('destacado');
-                    });
-
-                    cuadroActual = cuadroContenedor;
-                    cuadroActual.classList.add('destacado');
-                }
-                }
-            </script>
             </body>
             </html>
             <?php
