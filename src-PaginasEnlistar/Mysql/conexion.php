685ae2fd36f75ed943495c5fe8302e523961fa22
Alex Lizano <alexsanty10lm@gmail.com>
2024-01-02 23:51:45 -0500

    Se ha modificado la conexión

--- src-PaginasEnlistar/Mysql/conexion.php
+++ src-PaginasEnlistar/Mysql/conexion.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username="root";
     $password="root123";
-    $database="sistema_laboratorios";
+    $database="inv_laboratorio";
     $conn=mysqli_connect($servername, $username, $password, $database);
     if (!$conn){
         die("Error connecting ".mysqli_connect_error());
