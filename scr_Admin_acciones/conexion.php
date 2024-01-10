fb996a6b73f88c1e39c39cd3eac6cc3d6c792a55
Jefferson Poaquiza <jeffstalim111@gmail.com>
2024-01-10 12:05:57 -0500

    Se corrigue la conexion a la base de datos

--- scr_Admin_acciones/conexion.php
+++ scr_Admin_acciones/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $servername="localhost";
 $username="root";
-$password="admin";
+$password="";
 $database="inv_laboratorio";
 $conn=mysqli_connect($servername,$username,$password,$database);
 
